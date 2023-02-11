14fe769ccf1c0c50762daac3a50e806e5dd290ea
Marveeen <jmvroqueee@gmail.com>
2023-02-11 08:34:55 +0800

    added initial Home components

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Card from "../UI/Card";
+
+import classes from "./Home.module.css";
+
+const Home = () => {
+  return <Card className={classes.home}>Welcome back!</Card>;
+};
+
+export default Home;
