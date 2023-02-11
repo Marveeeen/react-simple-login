b0ce6300df6722e039e2171a172e276e4d0ef5b0
Marveeen <jmvroqueee@gmail.com>
2023-02-11 08:48:07 +0800

    added inital implementation for Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./components/Home";
+import MainHeader from "./components/Header";
 
 function App() {
   return (
     <>
+      <MainHeader />
       <main>
         <Home />
       </main>
